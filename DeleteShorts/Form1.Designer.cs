ef71493f3f32617537e2102bae384bd30d49313a
jidesheng6 <soeiz@soeiz-PC.SoeizTech.cn>
2020-08-25 15:35:19 +0800

    null

---
+++ DeleteShorts/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DeleteShorts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.StartButton.ForeColor = System.Drawing.Color.ForestGreen;
+            this.StartButton.Location = new System.Drawing.Point(86, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "开始";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(1);
+            this.label1.Size = new System.Drawing.Size(239, 64);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "本软件会替换您的桌面快捷方式图标箭头为透明，本操作可逆，但是作者不是特别想做这个功能，所以您看着办吧。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 105);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartButton);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DelShCuts-BYJI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DeleteShorts/RkeyMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DeleteShorts
+{
+    class RkeyMain
+    {
+
+        private void GetPathName(out string SystemDir, out string UProfile)
+        {
+            string SystemRootDir, UserProfileDir;
+            SystemRootDir = Environment.GetEnvironmentVariable("systemroot");
+            UserProfileDir = Environment.GetEnvironmentVariable("userprofile");
+            SystemDir = SystemRootDir;
+            UProfile = UserProfileDir;
+        }
+        public void RegIstryMethod()
+        {
+            string Shorts_Path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons";
+            RegistryKey Rkey0 = Registry.LocalMachine.OpenSubKey(Shorts_Path,true);
+            if (Rkey0 == null)
+            {
+                MessageBox.Show("系统检测到您的计算机注册表内没有Shell Icons项,将为您的计算机添加注册表项目，若发生杀毒软件提示，属于正常现象，请悉知。","提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                try
+                {
+                    RegistryKey Rkey1 = Registry.LocalMachine.CreateSubKey(Shorts_Path, true);
+                    if (Rkey1 != null)
+                    {
+                        string SystmDir, Udir;
+                        GetPathName(out SystmDir, out Udir);
+                        string Short_Value = $@"{SystmDir}\system32\imageres.dll,197";
+                        Rkey1.SetValue("29", Short_Value, RegistryValueKind.String);
+                        SetFileAttr();
+                        RestartExplorer();
+
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("请使用管理员身份运行","发生权限读取错误",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+                
+            }
+            else
+            {
+                if (Rkey0.GetValueNames().Contains("29") == true)
+                {
+                    MessageBox.Show("系统检测到您的计算机中已经被设置了去除快捷方式箭头，请不要再试", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Application.Exit();
+                    
+                }
+                else
+                {
+                    string SystmDir, Udir;
+                    GetPathName(out SystmDir, out Udir);
+                    string Short_Value = $@"{SystmDir}\system32\imageres.dll,197";
+                    Rkey0.SetValue("29", Short_Value,RegistryValueKind.String);
+                    MessageBox.Show("系统检测到您的设置不正确，已经将您的设置重新引导","提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    SetFileAttr();
+                    RestartExplorer();
+                   
+                }
+                
+            }
+            
+        }
+        private void SetFileAttr()
+        {
+
+            string SystmDir, Udir;
+            GetPathName(out SystmDir, out Udir);
+            string IconCacheDir = $@"{Udir}\AppData\Local\iconcache.db";
+            bool FileIsExit = File.Exists(IconCacheDir);
+            if (FileIsExit == true)
+            {
+                File.SetAttributes(IconCacheDir,FileAttributes.Normal);
+                File.Delete(IconCacheDir);
+            }
+            else
+            {
+                
+                
+            }
+        }
+        private void RestartExplorer()
+        {
+            string ProcessName = "explorer";
+            foreach (Process Pname in Process.GetProcessesByName(ProcessName))
+            {
+                if (Pname == null)
+                {
+                    MessageBox.Show("您的桌面进程已结束，系统将启动您的桌面进程", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process.Start("explorer.exe");
+                }
+                else
+                {
+                    Pname.Kill();
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
